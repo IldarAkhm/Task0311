1c94dbca3f854dff56bf5a719a2abbf5aeab2b69
IldarAkhm <ildar449@mail.ru>
2022-12-10 12:15:36 +0300

    commitfour

---
+++ Task2/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    
+    internal class Person
+    {
+        public delegate void Reaction(string m);
+        public event Reaction React;
+        public string name { get; set; }
+        public string hobby { get; set; }
+        public Person(string name, string hobby)
+        {
+            this.name = name;
+            this.hobby = hobby;
+        }
+        public void Check(string q)
+        {
+            if (q.ToLower() == hobby.ToLower())
+            {
+                React.Invoke($"{name} рад событию");
+            }
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
+     
+    
     internal class Program
-    {
+    { 
         static void Main(string[] args)
         {
+            List<Person> listperson = new List<Person>
+            {
+                new Person("Ильдар", "Кино"),
+                new Person("Илназ", "Сериал"),
+                new Person("Илья", "Комиксы")
+            };
+            Console.WriteLine("Введите название вашего события");
+            string even_t = Console.ReadLine();
+            foreach (var i in listperson)
+            {
+                if (i.hobby.ToLower() == even_t.ToLower())
+                {
+                    i.React += Message;
+                    i.Check(even_t);
+                }
+            }
+
+
+
+            Console.Read();
+         
         }
+        public static void Message(string msg) { Console.WriteLine(msg); }
     }
 }
